3652aea487919bf73b1804897fece22eb52add5c
Ibragim Dzaurov <100013546+IbragimDzaurov@users.noreply.github.com>
2022-10-12 10:51:35 +0300

    Add files via upload

---
+++ lab-2/main.py
@@ -0,0 +1,15 @@
+from lib.rectangle import Rectangle
+from lib.circle import Circle
+from lib.square import Square
+
+
+def main():
+    rect = Rectangle("синий", 2, 2)
+    cirсle = Circle("зеленый", 2)
+    square = Square("красный", 2)
+    print(rect)
+    print(cirсle)
+    print(square)
+
+if __name__ == "__main__":
+    main()
